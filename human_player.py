baa01aa9075f8080cdcc227a0b31d3827fa56ef8
YuraS <yurastepanenko@gmail.com>
2022-04-23 17:34:49 +0300

    create_human_player + fixed class human_player

--- human_player.py
+++ human_player.py
@@ -5,6 +5,3 @@ class Human_player(gamer.Gamer):
     def __init__(self, name, money):
         super().__init__(name, money)
 
-    # def __str__(self):
-    #     return f'Игрока зовут {self.name}. У него {self.money} денег!'
-
